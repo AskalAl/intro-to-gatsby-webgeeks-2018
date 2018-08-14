3dd6ec88fd75ee8ced07328b443bfacd63147af3
Josh Larson <jplhomer@gmail.com>
2018-08-14 13:05:48 -0500

    Add graphql demos

--- theme.js
+++ theme.js
@@ -2,13 +2,17 @@
 import { future } from 'mdx-deck/themes'
 import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
 import diff from 'react-syntax-highlighter/languages/prism/diff'
+import graphql from 'react-syntax-highlighter/languages/prism/graphql'
+import json from 'react-syntax-highlighter/languages/prism/json'
 
 export default {
   ...future,
   prism: {
     style: okaidia,
     languages: {
-      diff
+      diff,
+      graphql,
+      json
     }
   }
 }
