b30dc64b1222aa0dbbd6d425367a666c9773182d
Josh Larson <jplhomer@gmail.com>
2018-08-14 11:34:26 -0500

    Support diffs

--- theme.js
+++ theme.js
@@ -1,10 +1,14 @@
 // example theme.js
 import { future } from 'mdx-deck/themes'
 import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
+import diff from 'react-syntax-highlighter/languages/prism/diff'
 
 export default {
   ...future,
   prism: {
-    style: okaidia
+    style: okaidia,
+    languages: {
+      diff
+    }
   }
 }
